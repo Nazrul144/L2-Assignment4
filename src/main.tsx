b733ba931c0d4410aa769baa5158b3ef0dd1b26b
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-07-13 18:37:48 +0600

    Books routing added

--- src/main.tsx
+++ src/main.tsx
@@ -12,8 +12,20 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <h1>Home Components</h1>,
+    errorElement:<h1 className='font-bold text-2xl text-center'>Page Not found!</h1>,
     children: [
-      
+      {
+        path: '/',
+        element: <h1>Books</h1>
+      },
+      {
+        path: '/addBooks',
+        element: <h1>Add Books</h1>
+      },
+      {
+        path: '/booksSummery',
+        element: <h1>Books Summery</h1>
+      },
     ],
   },
 ]);
