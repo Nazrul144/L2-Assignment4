5eb775cb12db9b0e195a3be9cb4254302bdbefc9
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-16 03:26:07 +0600

    Add books added

--- src/components/pages/AddBooks.tsx
+++ src/components/pages/AddBooks.tsx
@@ -1,9 +1,79 @@
+import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
 
-
-const AddBooks = () => {
-  return (
-    <div>AddBooks</div>
-  )
+interface Book {
+  _id: string;
+  title: string;
+  author: string;
+  genre: string;
+  isbn: string;
+  description: string;
+  copies: number;
+  available: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
 
-export default AddBooks
+const AddBooks = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    fetch("https://library-management-api-five.vercel.app/api/books")
+      .then((res) => res.json())
+      .then((data) => setBooks(data.data))
+      .catch((error) => console.error("Error fetching books:", error));
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Title</TableHead>
+              <TableHead>Author</TableHead>
+              <TableHead>Genre</TableHead>
+              <TableHead className="text-right">ISBN</TableHead>
+              <TableHead className="text-right">Copies</TableHead>
+              <TableHead className="text-right">Status</TableHead>
+              <TableHead className="text-right">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {books.map((book) => (
+              <TableRow key={book._id}>
+                <TableCell className="font-medium">{book.title}</TableCell>
+                <TableCell>{book.author}</TableCell>
+                <TableCell>{book.genre}</TableCell>
+                <TableCell className="text-right">{book.isbn}</TableCell>
+                <TableCell className="text-right">{book.copies}</TableCell>
+                <TableCell className="text-right">
+                  {book.available ? "Yes" : "No"}
+                </TableCell>
+                <TableCell className="text-right space-x-2">
+                  <button className="btn btn-sm btn-primary">Edit</button>
+                  <button className="btn btn-sm btn-error">Delete</button>
+                  <button className="btn btn-sm btn-success" disabled={!book.available}>
+                    Borrow
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default AddBooks;
