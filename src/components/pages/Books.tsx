a77c9f1f424b99c51546a05cfd284f7325e14f14
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-16 02:40:13 +0600

    All books are fetched

--- src/components/pages/Books.tsx
+++ src/components/pages/Books.tsx
@@ -1,9 +1,55 @@
+import React, { useEffect, useState } from "react";
 
-
-const Books = () => {
-  return (
-    <div>Books</div>
-  )
+interface Book {
+  _id: string;
+  title: string;
+  author: string;
+  genre: string;
+  isbn: string;
+  description: string;
+  copies: number;
+  available: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
 
-export default Books
+const Books: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    fetch("https://library-management-api-five.vercel.app/api/books")
+      .then((res) => res.json())
+      .then((data) => setBooks(data.data))
+      .catch((error) => console.error("Error fetching books:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center my-4">Books List</h1>
+      <table className="table w-full">
+        <thead className="font-bold text-xl text-black">
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Genre</th>
+            <th>Copies</th>
+            <th>Available</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr key={book._id}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.genre}</td>
+              <td>{book.copies}</td>
+              <td>{book.available ? "Yes" : "No"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Books;
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -111,7 +111,7 @@ const Navbar = () => {
                 }`
               }
             >
-              BooksSummary
+              Borrow Summary
             </NavLink>
 
 
